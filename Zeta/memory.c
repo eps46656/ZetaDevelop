4df9e74a0c1214c0c0e41e7910d459d4acbfef99
eps46656 <eps46656@gmail.com>
2024-11-23 16:00:28 +0800

    0

---
+++ Zeta/memory.c
@@ -0,0 +1,5 @@
+#include "memory.h"
+
+Zeta_Allocator* zeta_cas_allocator = NULL;
+
+Zeta_Allocator* zeta_tree_allocator = NULL;
---
+++ Zeta/memory.h
@@ -0,0 +1,9 @@
+#include "allocator.h"
+
+ZETA_ExternC_Beg;
+
+extern Zeta_Allocator* zeta_cas_allocator;
+
+extern Zeta_Allocator* zeta_tree_allocator;
+
+ZETA_ExternC_End;
